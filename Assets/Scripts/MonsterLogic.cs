384ae474bba23f6e0031bd603708889881472f66
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-03-14 21:45:36 +0800

    feat: now monster can do physics interactions

--- Assets/Scripts/MonsterLogic.cs
+++ Assets/Scripts/MonsterLogic.cs
@@ -14,39 +14,55 @@ public class MonsterLogic : MonoBehaviour
 
     [SerializeField]
     private Detector detector, buffer;
+    [SerializeField]
+    private Transform body;
+    private PlayerLogic player;
+
+    private Vector3 velocity;
     private new Rigidbody rigidbody;
 
-    private Vector3 velocity = Vector3.zero;
-
+    const float SPEED = 3.0f;
     const float ELEVATE_SPEED = 1.0f;
     const float FLOAT_AMPLITUDE = 0.6f;
-    const float FALL_SPEED = 0.2f;
+    const float FALL_SPEED = 2f;
 
     private void Start()
     {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerLogic>();
         rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        velocity.y = math.sin(Time.time) * FLOAT_AMPLITUDE;
-
         switch (monsterState)
         {
             case MonsterState.IDLE:
-                if (!buffer.detected) transform.parent.Translate(FALL_SPEED * Time.deltaTime * Vector3.down);
+                if (!buffer.detected) velocity.y = -FALL_SPEED;
+                else velocity.y = 0;
                 if (detector.detected) monsterState = MonsterState.ELEVATE;
                 break;
             case MonsterState.ELEVATE:
-                transform.parent.Translate(ELEVATE_SPEED * Time.deltaTime * Vector3.up);
+                velocity.y = ELEVATE_SPEED;
                 if (!detector.detected) monsterState = MonsterState.IDLE;
                 break;
         }
+
+        var pos = player.transform.position;
+        pos.y = transform.position.y;
+        transform.LookAt(pos);
+
+        var distance = player.transform.position - transform.position;
+        var direction = distance.normalized;
+        velocity.x = direction.x * SPEED;
+        velocity.z = direction.z * SPEED;
     }
 
     private void FixedUpdate()
     {
         rigidbody.velocity = velocity;
-        velocity = rigidbody.velocity;
+
+        var bodyVelocity = Vector3.zero;
+        bodyVelocity.y = math.sin(Time.time) * FLOAT_AMPLITUDE;
+        body.Translate(bodyVelocity * Time.deltaTime);
     }
 }
