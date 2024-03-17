fc3f9df914496ed9a32c694f99b74213b42e53bb
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-03-17 12:03:31 +0800

    chore: disable damage field gizmos

--- Assets/Scripts/DamageField.cs
+++ Assets/Scripts/DamageField.cs
@@ -6,12 +6,6 @@ public class DamageField : MonoBehaviour
     public bool isEnemy;
     public float dizzyTime;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = new(1.0f, 0, 0, 0.5f);
-        Gizmos.DrawSphere(transform.position, transform.lossyScale.x / 2);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (isEnemy && other.CompareTag("Player"))
